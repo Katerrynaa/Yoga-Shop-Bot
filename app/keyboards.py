cff82bcb6ab053f3a508871657d419cab7ffa4b7
Katerrynaa <136275888+Katerrynaa@users.noreply.github.com>
2024-08-28 13:22:28 +0200

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -30,25 +30,3 @@ async def items(category_id):
         keyboard.add(InlineKeyboardButton(text=item.name, callback_data=f'item_{item.id}'))
     keyboard.add(InlineKeyboardButton(text='Main page', callback_data='main'))
     return keyboard.adjust(2).as_markup()
-
-
-
-
-
-
-
-
-# from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, 
-                        #    InlineKeyboardButton, InlineKeyboardMarkup)
-
-
-# catalog = InlineKeyboardMarkup(inline_keyboard=[
-#     [InlineKeyboardButton(text='T-shirts', callback_data='t-shirts')],
-#     [InlineKeyboardButton(text='Jeans', callback_data='jeans')],
-#     [InlineKeyboardButton(text='Sneakers', callback_data='sneakers')],
-#     [InlineKeyboardButton(text='Sweaters', callback_data='sweaters')]])
-
-
-# get_number = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Send a number', 
-#                                                            request_contact=True)]],
-#                                                            resize_keyboard=True)
